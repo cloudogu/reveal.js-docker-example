55fb642ba6a79ff6f9abe184fe3500ef340b72c7
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2020-06-26 12:39:31 +0200

    startPresentation.sh: Open browser on success

--- startPresentation.sh
+++ startPresentation.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+set -o errexit -o nounset -o pipefail
+set -o monitor # Job Control, needed for "fg"
+
 echo "Starting presentation on localhost:8000" 
 
-docker run --rm \
+CONTAINER=$(docker run --rm --detach \
     -v $(pwd)/docs/slides:/reveal/docs/slides \
     -v $(pwd)/resources:/resources \
     -e TITLE="$(grep -r 'TITLE' Dockerfile | sed "s/.*TITLE='\(.*\)'.*/\1/")" \
     -p 8000:8000 -p 35729:35729 \
-    cloudogu/reveal.js:$(head -n1 Dockerfile | sed 's/.*:\([^ ]*\) .*/\1/')-dev
+    cloudogu/reveal.js:$(head -n1 Dockerfile | sed 's/.*:\([^ ]*\) .*/\1/')-dev)
+
+# Print logs in background while waiting for container to come up
+docker attach ${CONTAINER} &
+
+until $(curl -s -o /dev/null --head --fail localhost:8000); do sleep 1; done
+
+# Open Browser
+xdg-open http://localhost:8000
+
+# Bring container to foreground, so it can be stopped using ctrl+c. 
+# But don't output "docker attach ${CONTAINER}"
+fg > /dev/null
